a801f0348b2858fc3f215ee5f1dba9426f5e8b97
Bini Babu <bini.babu@dbizsolution.com>
2024-09-26 12:06:49 +0530

    Object reference

--- src/com/bini/babu/ObjectReferenceDemo.java
+++ src/com/bini/babu/ObjectReferenceDemo.java
@@ -2,8 +2,14 @@ package com.bini.babu;
 
 public class ObjectReferenceDemo {
 
+	int x;
+	ObjectReferenceDemo(){
+		
+	}
+	
 	public static void main(String[] args) {
-
+		ObjectReferenceDemo ord = new ObjectReferenceDemo();
+		System.out.println(ord.x);
 	}
 
 }
